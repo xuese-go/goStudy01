64db5a3f694800d1bbd54f9b66aef711e7ca4a3a
Gogs <gogs@fake.local>
2020-12-16 12:01:57 +0800

    #1.添加log demo转载地址 #2.根据情况调整log格式

--- log/log.go
+++ log/log.go
@@ -1,5 +1,6 @@
 /**
 demo来源于网络
+https://blog.csdn.net/lfhlzh/article/details/106151419
 */
 package log
 
@@ -13,24 +14,16 @@ import (
 	"time"
 )
 
-func init() {
-	InitLog("./info.log", "./error.log", zap.InfoLevel)
-	defer logger.Sync()
-}
-
-//只能输出结构化日志，但是性能要高于 SugaredLogger
-var logger *zap.Logger
-
-//可以输出 结构化日志、非结构化日志。性能茶语 zap.Logger，具体可见上面的的单元测试
 var SugarLogger *zap.SugaredLogger
 
+func init() {
+	InitLog("./info.log", "./warn.log", "./error.log")
+	defer SugarLogger.Sync()
+}
+
 // 初始化日志 logger
-func InitLog(logPath, errPath string, logLevel zapcore.Level) {
+func InitLog(logPath, warnPath, errPath string) {
 	config := zapcore.EncoderConfig{
-		MessageKey:   "msg",                       //结构化（json）输出：msg的key
-		LevelKey:     "level",                     //结构化（json）输出：日志级别的key（INFO，WARN，ERROR等）
-		TimeKey:      "ts",                        //结构化（json）输出：时间的key（INFO，WARN，ERROR等）
-		CallerKey:    "file",                      //结构化（json）输出：打印日志的文件对应的Key
 		EncodeLevel:  zapcore.CapitalLevelEncoder, //将日志级别转换成大写（INFO，WARN，ERROR等）
 		EncodeCaller: zapcore.ShortCallerEncoder,  //采用短文件路径编码输出（test/main.go:14	）
 		EncodeTime: func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
@@ -42,45 +35,42 @@ func InitLog(logPath, errPath string, logLevel zapcore.Level) {
 	}
 	//自定义日志级别：自定义Info级别
 	infoLevel := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
-		return lvl < zapcore.WarnLevel && lvl >= logLevel
+		return lvl == zapcore.InfoLevel
 	})
 
 	//自定义日志级别：自定义Warn级别
 	warnLevel := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
-		return lvl >= zapcore.WarnLevel && lvl >= logLevel
+		return lvl == zapcore.WarnLevel
+	})
+
+	//自定义日志级别：自定义Warn级别
+	errLevel := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= zapcore.ErrorLevel
 	})
 
 	// 获取io.Writer的实现
 	infoWriter := getWriter(logPath)
-	warnWriter := getWriter(errPath)
+	warnWriter := getWriter(warnPath)
+	errWriter := getWriter(errPath)
 
 	// 实现多个输出
 	core := zapcore.NewTee(
-		zapcore.NewCore(zapcore.NewConsoleEncoder(config), zapcore.AddSync(infoWriter), infoLevel),                         //将info及以下写入logPath，NewConsoleEncoder 是非结构化输出
-		zapcore.NewCore(zapcore.NewConsoleEncoder(config), zapcore.AddSync(warnWriter), warnLevel),                         //warn及以上写入errPath
-		zapcore.NewCore(zapcore.NewJSONEncoder(config), zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout)), logLevel), //同时将日志输出到控制台，NewJSONEncoder 是结构化输出
+		zapcore.NewCore(zapcore.NewConsoleEncoder(config), zapcore.AddSync(infoWriter), infoLevel),                                   //写入文件
+		zapcore.NewCore(zapcore.NewConsoleEncoder(config), zapcore.AddSync(warnWriter), warnLevel),                                   //写入文件
+		zapcore.NewCore(zapcore.NewConsoleEncoder(config), zapcore.AddSync(errWriter), errLevel),                                     //写入文件
+		zapcore.NewCore(zapcore.NewJSONEncoder(config), zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout)), zapcore.DebugLevel), //将日志输出到控制台
 	)
-	logger = zap.New(core, zap.AddCaller(), zap.AddStacktrace(zap.WarnLevel))
+	logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zap.WarnLevel))
 	SugarLogger = logger.Sugar()
 }
 
-//func getWriter(filename string) io.Writer {
-//	return &lumberjack.Logger{
-//		Filename:   filename,
-//		MaxSize:    10,    //最大M数，超过则切割
-//		MaxBackups: 5,     //最大文件保留数，超过就删除最老的日志文件
-//		MaxAge:     30,    //保存30天
-//		Compress:   false, //是否压缩
-//	}
-//}
-
+//设置输出文件位置,以及分割方案
 func getWriter(filename string) io.Writer {
-	// 生成rotatelogs的Logger 实际生成的文件名 filename.YYmmddHH
 	// filename是指向最新日志的链接
 	hook, err := rotatelogs.New(
 		filename+".%Y%m%d%H",
 		rotatelogs.WithLinkName(filename),
-		rotatelogs.WithMaxAge(time.Hour*24*30),    // 保存30天
+		rotatelogs.WithMaxAge(time.Hour*24*15),    // 保存15天
 		rotatelogs.WithRotationTime(time.Hour*24), //切割频率 24小时
 	)
 	if err != nil {
